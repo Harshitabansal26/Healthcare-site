304e472f2927ea2f2fee573d25489814fca270be
Harshita <harshitabansal3142@gmail.com>
2024-10-21 11:46:26 +0530

    commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Healthcare-site
+
+in server folder 
+npm init
+
+package name : server 
+
+npm i jaha aapka package.json h ussi folder m 
--- server/server.js
+++ server/server.js
@@ -1,1 +1,25 @@
-sdfv
+const express=require("express")
+const connectDb = require("./config/dbConnection");
+const errorHandler = require("./middleware/errorHandler");
+const cors = require("cors");
+
+connectDb();
+
+const app=express();
+
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+
+app.get("/",(req,res)=>{
+    res.send("working");
+})
+
+
+//APP CONFIG START
+app.listen(port,()=>{
+    console.log(`Server has started on port http://localhost:${port}`)
+
+});
