9e0276600064236476797376bbf517258137d8fa
Harshita <harshitabansal3142@gmail.com>
2024-10-23 11:40:20 +0530

    hbs

--- server/server.js
+++ server/server.js
@@ -1,30 +1,125 @@
-const express=require("express")
-const connectDb = require("./config/dbConnection");
-const errorHandler = require("./middleware/errorHandler");
-const cors = require("cors");
+// const express=require("express")
+// const connectDb = require("./config/dbConnection");
+// const errorHandler = require("./middleware/errorHandler");
+// const cors = require("cors");
 
-//env filr config
+// //env filr config
 
-const dotenv = require("dotenv");
+// const dotenv = require("dotenv");
+// dotenv.config();
+
+// connectDb();
+
+// const app=express();
+
+// const port = process.env.PORT || 5000;
+
+// app.set('view engine' , 'hbs');
+// app.use(express.json());
+// app.use(cors());
+
+
+// app.get("/",(req,res)=>{
+//     res.send("working");
+// })
+
+// app.get("/home",(req,res)=>{
+//     res.render("home",{
+//         username:"Harshita",
+//         posts:"abc"
+
+//     })
+// })
+
+// app.get("/alluser",(req,res)=>{
+//     res.render("home",{
+//         username:"Harshita",
+//         posts:"abc"
+
+//     })
+// })
+
+// // app.get()
+
+
+// //APP CONFIG START
+// app.listen(port,()=>{
+//     console.log(`Server has started on port http://localhost:${port}`)
+
+// });
+
+const express = require ("express")
+const connectDb = require("./config/dbConnection")
+const mongoose=require("mongoose")
+const errorHandler = require("./middleware/errorHandler")
+
+const cors = require("cors")
+
+//env file config
+const dotenv=require("dotenv")
 dotenv.config();
 
-connectDb();
-
-const app=express();
-
-const port = process.env.PORT || 5000;
-
-app.use(express.json());
-app.use(cors());
 
 
-app.get("/",(req,res)=>{
-    res.send("working");
+// connectDb()
+
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(express.json())
+app.use(cors())
+app.set("view engine", "hbs");
+
+app.use(errorHandler)
+
+
+
+
+
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/HealthCare System', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+
+const mySchema=new mongoose.Schema({
+    name:String,
+    age:Number
+})
+
+const User= mongoose.model('User',mySchema)
+app.get("/", (req,res)=>{
+    res.send("Hello World")
+})
+
+app.get("/home",(req,res)=>{
+
+    res.render("home",{
+
+    })
+})
+
+app.get("/users",(req,res)=>{
+    res.render("users",{
+
+        people:[
+            {
+                username:"himansh",
+                age:20
+            },
+            {
+                username:"mahajan",
+                age:21
+            }
+        ]
+
+    })
 })
 
 
-//APP CONFIG START
-app.listen(port,()=>{
-    console.log(`Server has started on port http://localhost:${port}`)
-
-});
+app.listen(port, ()=>{
+    console.log("Server is running on port 3001")
+})
